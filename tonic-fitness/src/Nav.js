cd44e9461ec81e57d798b5e2eedbd2da70268f67
ZumDeWald <zumdewald@gmail.com>
2018-12-04 21:57:44 -0600

    Login words in place and styled

--- tonic-fitness/src/Nav.js
+++ tonic-fitness/src/Nav.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 class Nav extends Component {
   render() {
     return (
-      <section id="nav-container">
+      <nav id="nav-container">
         <header id="header">
-          <h1 className="nav-header">Tonic Fitness</h1>
+          <h1 id="nav-header">Tonic Fitness</h1>
+          <div className="nav-login">
+            <span className="nav-login-item">Sign in</span>
+            <span className="nav-login-item">Sign up</span>
+          </div>
         </header>
-      </section>
+      </nav>
     );
   }
 }
