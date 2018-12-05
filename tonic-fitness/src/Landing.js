f92f819adac8866c32bf3b651d7b14065bf0cc2a
ZumDeWald <zumdewald@gmail.com>
2018-12-05 06:43:29 -0600

    Adding icons for landing

--- tonic-fitness/src/Landing.js
+++ tonic-fitness/src/Landing.js
@@ -5,7 +5,15 @@ class Landing extends Component {
     return (
       <section id="landing-container" className="flex-col">
         <div className="landing-display flex-col">
-        <img className="placeholder" src="./placeholder.png" alt="Placeholder Img" />
+          <article className="landing-display-item flex-row">
+            <i className="fas fa-list-ol fa-8x"></i>
+          </article>
+          <article className="landing-display-item flex-row">
+            <i className="fas fa-apple-alt fa-8x"></i>
+          </article>
+          <article className="landing-display-item flex-row">
+            <i className="fas fa-chart-bar fa-8x"></i>
+          </article>
         </div>
       </section>
     );
@@ -13,3 +21,5 @@ class Landing extends Component {
 }
 
 export default Landing;
+
+// <img className="placeholder" src="./placeholder.png" alt="Placeholder Img" />
