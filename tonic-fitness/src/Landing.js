dabddfefd4c8a7c2e51431b399f840ee8316e0ac
ZumDeWald <zumdewald@gmail.com>
2018-12-05 06:47:19 -0600

    Icons and words on Landing outlined

--- tonic-fitness/src/Landing.js
+++ tonic-fitness/src/Landing.js
@@ -4,15 +4,21 @@ class Landing extends Component {
   render() {
     return (
       <section id="landing-container" className="flex-col">
-        <div className="landing-display flex-col">
-          <article className="landing-display-item flex-row">
+        <div className="landing-display flex-row">
+          <article className="landing-display-item flex-col">
             <i className="fas fa-list-ol fa-8x"></i>
+            <p className="heading-text"> Set Goals
+            </p>
           </article>
-          <article className="landing-display-item flex-row">
+          <article className="landing-display-item flex-col">
             <i className="fas fa-apple-alt fa-8x"></i>
+            <p className="heading-text"> Log Food
+            </p>
           </article>
-          <article className="landing-display-item flex-row">
+          <article className="landing-display-item flex-col">
             <i className="fas fa-chart-bar fa-8x"></i>
+            <p className="heading-text"> See Results!
+            </p>
           </article>
         </div>
       </section>
