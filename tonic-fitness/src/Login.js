7d9c6dcd592346223bca9f3061832721a00f75c0
ZumDeWald <zumdewald@gmail.com>
2018-12-05 22:33:03 -0600

    Basic function written to open Login

--- tonic-fitness/src/Login.js
+++ tonic-fitness/src/Login.js
@@ -4,7 +4,7 @@ import Signup from './Signup.js';
 
 const Login = () => {
   return (
-    <aside id="login-container" className="flex-col">
+    <aside id="login-container" className="hide flex-col">
       <div className="login-nav">
         <span className="login-nav-item">Sign In</span>
         <span className="login-nav-item">Sign Up</span>
--- tonic-fitness/src/Nav.js
+++ tonic-fitness/src/Nav.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+const openLogin = () => {
+  let loginContainer = document.getElementById("login-container");
+  loginContainer.classList.toggle("hide");
+}
+
 const Nav = () => {
   return (
     <nav id="nav-container">
       <header id="header">
         <h1 id="nav-header">Tonic Fitness</h1>
         <div className="nav-login">
-          <span className="nav-login-item">Sign in</span>
-          <span className="nav-login-item">Sign up</span>
+          <span className="nav-login-item"
+                onClick= {openLogin}
+                >Sign in</span>
+          <span className="nav-login-item"
+                onClick= {openLogin}
+                >Sign up</span>
         </div>
       </header>
     </nav>
