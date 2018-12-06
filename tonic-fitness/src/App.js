bbca4745da0125776655ff708e7721d9f0680605
ZumDeWald <zumdewald@gmail.com>
2018-12-05 21:59:36 -0600

    Create box for signup modal

--- tonic-fitness/src/App.js
+++ tonic-fitness/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Nav from './Nav.js';
+import Signup from './Signup';
 import Landing from './Landing.js';
 import Goals from './Goals.js';
 
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div id="app-container">
         <Nav />
+        <Signup />
         <Landing />
         <Goals />
       </div>
--- tonic-fitness/src/Signup.js
+++ tonic-fitness/src/Signup.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <aside id="signup-container">
+    <aside id="signup-container" className="flex-col">
+      <div className="signup-modal">
+      </div>
     </aside>
   );
 }
