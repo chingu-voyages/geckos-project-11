89e494089babd8b94668d51e60fefcb3dbb97a76
ZumDeWald <zumdewald@gmail.com>
2018-12-06 21:07:37 -0600

    Added close to signup modal

--- tonic-fitness/src/Login.js
+++ tonic-fitness/src/Login.js
@@ -5,14 +5,17 @@ import Signup from './Signup.js';
 const Login = (props) => {
   return (
     <aside id="login-container"
-           className="hide flex-col"
-           onClick={props.closeLogin}>
+           className="hide flex-col">
       <div className="login-nav">
-        <span className="login-nav-item">Sign In</span>
         <span className="login-nav-item">Sign Up</span>
+        <span className="login-nav-item">Sign In</span>
       </div>
       <div className="login-body flex-col">
-      <Signup />
+        <h4 className="login-close"
+            onClick={props.closeLogin}>
+          X close
+        </h4>
+        <Signup />
       </div>
     </aside>
   );
