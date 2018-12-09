ea506eb2c83a10d0ee5a879400aa9048a9c74898
ZumDeWald <zumdewald@gmail.com>
2018-12-09 14:59:41 -0600

    Initial form created for Signup

--- tonic-fitness/src/Signup.js
+++ tonic-fitness/src/Signup.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Signup = () => {
   return (
     <div className="login-body flex-col">
-      SIGNUP
+      <form action="" method="post" id="signup-form">
+        <div className="form-field">
+          Name:
+          <input type="text" name="name" id="form-name" placeholder="Skinnyboy Thompson" required />
+        </div>
+        <div className="form-field">
+          Password:
+          <input type="password" name="name" id="form-password" minlength="6" maxlength="20" placeholder="6-20 characters" required />
+        </div>
+        <div className="form-field">
+          Location:
+          <input type="text" name="name" id="form-loc" placeholder="Where are you from?" required />
+        </div>
+      </form>
     </div>
   );
 }
