b4d5a49fd6226748b2e6ad7d12385da981338d09
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-12-10 19:49:44 -0500

    adding goals goalsList

--- tonic-fitness/src/Goals.js
+++ tonic-fitness/src/Goals.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Goals = () => {
+class Goals extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goalsList: [
+        'I want to lose...',
+        'By XX-XX-XXXX',
+        'I want to fit'
+      ]
+    }
+
+  }
+ 
+  render () {
     return (
       <div className="goals">
-      Goals!!
+
+        <ul>
+          {this.state.goalsList.map(item => {
+            return (
+            <li>{item}</li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
+}
 
 export default Goals;
