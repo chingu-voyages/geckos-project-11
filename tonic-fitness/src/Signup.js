9e7aa011a473c5901b2ce1b257b44ea7ee02ec63
ZumDeWald <zumdewald@gmail.com>
2018-12-10 21:39:53 -0600

    Style and spacing to Signup Form

--- tonic-fitness/src/Signup.js
+++ tonic-fitness/src/Signup.js
@@ -4,15 +4,15 @@ const Signup = () => {
   return (
     <div className="login-body flex-col">
       <form action="" method="post" id="signup-form">
-        <div className="form-field">
+        <div className="form-field heading-text">
           Name:
           <input type="text" name="name" id="name" placeholder="Skinnyboy Thompson" required />
         </div>
-        <div className="form-field">
+        <div className="form-field heading-text">
           Password:
           <input type="password" name="name" id="password" minlength="6" maxlength="20" placeholder="6-20 characters" required />
         </div>
-        <div className="form-field">
+        <div className="form-field heading-text">
           Location:
           <input type="text" name="name" id="loc" placeholder="Where are you from?" required />
         </div>
