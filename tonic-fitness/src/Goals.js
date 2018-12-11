7aebb8096017b63ffa1e7f92bbc71fcef99f82fa
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-12-10 20:51:57 -0500

    adding list

--- tonic-fitness/src/Goals.js
+++ tonic-fitness/src/Goals.js
@@ -7,21 +7,32 @@ class Goals extends Component  {
       goalsList: [
         'I want to lose...',
         'By XX-XX-XXXX',
-        'I want to fit'
+        'I want to fit into...'
       ]
     }
+    this.generateList = this.generateList.bind(this);
+  }
 
+  
+  generateList = e => {
+    let goal = this.state.goalsList;
+    if(e.charCode == 13) {
+      goal.push(e.target.value)
+      e.target.value = '';
+      this.setState({goalsList:goal})
+    }
   }
  
   render () {
+    const {goalsList} = this.state;
     return (
       <div className="goals">
         <h1>My Goals</h1>
-        <input 
-        className='textGoal' type='text' placeholder='Type your goal'>
+        <input
+        className='textGoal' type='text' placeholder='Type your goal' onKeyPress={this.generateList}>
         </input>
         <ul>
-          {this.state.goalsList.map(item => {
+          {goalsList.map(item => {
             return (
             <li>{item}</li>
             )
