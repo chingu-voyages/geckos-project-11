895419841408a39695f69ef237e6fa3e6a8f716c
ZumDeWald <zumdewald@gmail.com>
2018-12-11 21:01:07 -0600

    Change login import in Nav

--- tonic-fitness/src/components/Nav.js
+++ tonic-fitness/src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Login from './Login.js';
+import Login from './Login';
 
 const openLogin = () => {
   let loginContainer = document.getElementById("login-container");
