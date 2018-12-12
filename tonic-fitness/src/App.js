b268cb5ae0d8658fb4c0f9901bb62e5701adcca3
ZumDeWald <zumdewald@gmail.com>
2018-12-11 23:07:01 -0600

    Add conditional route to Login

--- tonic-fitness/src/App.js
+++ tonic-fitness/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Nav from './components/Nav.js';
+import Login from './components/Login.js';
 import Landing from './components/Landing.js';
 import Goals from './components/Goals.js';
 import Log from './components/Log.js';
@@ -11,7 +12,10 @@ import './stylesheets/App.scss';
 
 class App extends Component {
 
-  //Store last location for Login modal
+  //Store last location for Login modal,
+  //This is needed in order to allow the path
+  //to change and render Login but keep the
+  //current page displayed underneath (modal style)
   previousLocation = this.props.location;
 
   componentWillUpdate(nextProps) {
@@ -27,6 +31,16 @@ class App extends Component {
   }
 
   render() {
+    let { location } = this.props;
+
+    //Boolean to determine if the Login modal
+    //is showing
+    let modalShowing = !!(
+      location.state &&
+      location.state.modal &&
+      this.previousLocation !== location
+    );
+
     return (
       <div id="app-container">
         <Nav />
@@ -36,6 +50,7 @@ class App extends Component {
           <Route path='/log' component={Log}/>
           <Route path='/results' component={Results}/>
         </Switch>
+        {modalShowing ? <Route path="/login" component={Login} /> : null}
       </div>
     );
   }
