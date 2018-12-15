f23d9d75b94af3c6d39e0322786b6c1c79346d96
ZumDeWald <zumdewald@gmail.com>
2018-12-14 21:44:31 -0600

    Change icon sizes for mobile on Landing

--- tonic-fitness/src/components/Landing.js
+++ tonic-fitness/src/components/Landing.js
@@ -7,21 +7,21 @@ const Landing = () => {
         <div className="display flex-row">
           <article className="flex-col">
             <Link to="/goals">
-              <i className="fas fa-list-ol fa-8x"></i>
+              <i className="fas fa-list-ol"></i>
             </Link>
             <p className="heading-text"> Set Goals
             </p>
           </article>
           <article className="flex-col">
             <Link to="/log">
-              <i className="fas fa-apple-alt fa-8x"></i>
+              <i className="fas fa-apple-alt"></i>
             </Link>
             <p className="heading-text"> Log Food
             </p>
           </article>
           <article className="flex-col">
             <Link to="results">
-              <i className="fas fa-chart-bar fa-8x"></i>
+              <i className="fas fa-chart-bar"></i>
             </Link>
             <p className="heading-text"> See Results!
             </p>
