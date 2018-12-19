71d91ad79c1658a4f8dc645fef739d3487a2a1aa
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-12-18 20:01:35 -0500

    remove ul and lis from the generateList function.  Remove goals, and simple UI object

--- tonic-fitness/src/components/Goals.js
+++ tonic-fitness/src/components/Goals.js
@@ -4,11 +4,6 @@ class Goals extends Component  {
   constructor(props) {
     super(props);
     this.state = {
-      goalsList: [
-        'I want to lose...',
-        'By XX-XX-XXXX',
-        'I want to fit into...'
-      ],
       UI: {
         weight: '',
         lose: '',
@@ -23,44 +18,23 @@ class Goals extends Component  {
   }
 
   generateList = e => {
-    let goal = this.state.goalsList;
-    let currentUI = this.state.UI;
+    let uiGoals = this.state.UI;
 
     let goalType = this.optionSelected() + '';
-    if((goal[0] === 'I want to lose...' || goal[0] === 'By XX-XX-XXXX' || goal[0] === 'I want to fit into...') && e.charCode === 13) {
-        goal = [];
-        goal.push({[goalType]: e.target.value, ['id']:0});
-        this.updateUI(goalType, e.target.value);
+    if(e.charCode === 13) {
+        uiGoals[goalType] = e.target.value;
+        this.setState({UI:uiGoals});
         e.target.value = '';
-        this.props.renderApp(goal);
-        this.setState({goalsList:goal});
-      } else if (e.charCode === 13) {
-          goal.push({[goalType]: e.target.value, ['id']:goal.length});
-          this.updateUI(goalType, e.target.value);
-          e.target.value = '';
-          this.props.renderApp(goal);
-          this.setState({goalsList:goal});  
+        this.props.renderApp(uiGoals);
     }
   }
 
-  deleteGoal = id => {
-    let goal = this.state.goalsList;
-    let updatedList = goal.filter(item => item.id !== id);
-    return this.setState({goalsList:updatedList});
-  }
 
   updateUI = (e, userInput) => {
     let currentUI = this.state.UI;
     currentUI[e] = userInput;
-    console.log(currentUI)
-    console.log(e)
-    console.log(userInput)
     this.setState({UI: currentUI});
   }
-
-// current weight
-
-// weight loss goal
 
   render () {
     const {goalsList} = this.state;
@@ -76,16 +50,7 @@ class Goals extends Component  {
           <option value='lose' >Ideal Weight</option>
           <option value='by'>By</option>
           </select>
-        <ul>
-          {goalsList.map(item => {
-            return (
-              <div>
-                <li onClick={(this.deleteGoal.bind(this, item.id))}>{item.weight}</li>
-                <li onClick={(this.deleteGoal.bind(this, item.id))}>{item.lose}</li>
-              </div>
-            )
-          })}
-        </ul>
+        
         <section id="landing-container" className="flex-col">
         <div className="display flex-row">
           <article className="flex-col"> 
