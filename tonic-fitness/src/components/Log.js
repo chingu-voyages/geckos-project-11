0dfba17f84eda707bbe57c427cd63e1d222fe3c0
ZumDeWald <zumdewald@gmail.com>
2018-12-19 21:39:24 -0600

    First setup of Log

--- tonic-fitness/src/components/Log.js
+++ tonic-fitness/src/components/Log.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Log = () => {
-    return (
-        <div>
-            Food-log page.
-        </div>
-    )
+  return (
+    <div id="log">
+      <h3 className="title flex-col">
+        Hello World!
+      </h3>
+    </div>
+  )
 }
 
 export default Log;
