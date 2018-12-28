cf1502f432ee06bb2e0200a7c414d8cdcf215483
Diona <diona.david@gmail.com>
2018-12-28 14:22:46 -0500

    Add location field to user schema

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  location: {
+    type: String,
+    required: true
+  },
   date: {
     type: Date,
     default: Date.now
--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,8 @@ router.post("/register", (req, res) => {
     const newUser = new User({
           name: req.body.name,
           email: req.body.email,
-          password: req.body.password
+          password: req.body.password,
+          location: req.body.location
         });
   // Hash password before saving in database
         bcrypt.genSalt(10, (err, salt) => {
