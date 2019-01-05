7cbf04afcd1fadd70641e5dceee7be3eb371d293
kfarfan <34223564+kfarfan@users.noreply.github.com>
2019-01-04 20:24:07 -0500

    adding basic formula for testing using fake data, will use real data with database

--- tonic-fitness/src/components/Results.js
+++ tonic-fitness/src/components/Results.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
 const Results = (props) => {
+    let maintenance = 2000;
+    let totalCalories = 1750;
+    let defecit = maintenance - totalCalories;
+    let target = (props.weight - props.goal) * 3500;
+    let pace = Math.round(target/defecit);
     return (
         <div id='results'>
            {props.weight}
            {props.goal}
            {props.by}
 
-            <section id="landing-container" className="flex-col">
+           
+
+        <section id="landing-container" className="flex-col">
+        <h1>Maintenance 2000 calories</h1>
         <div className="display flex-row">
           <article className="flex-col">
               <i className="fas fa-weight"></i>
-            <p className="heading-text"> Calories today: </p>
+            <p className="heading-text"> Calories today: {totalCalories} </p>
           </article>
           <article className="flex-col">
               <i className="fas fa-chart-line"></i>
-            <p className="heading-text"> Current Pace: </p>
+            <p className="heading-text"> Current Pace: {pace} days</p>
           </article>
           <article className="flex-col">
               <i className="fas fa-clock"></i>
