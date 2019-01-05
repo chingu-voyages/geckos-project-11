514a2b8f260d43f61ebc47b04daf83cbcd232cb1
kfarfan <34223564+kfarfan@users.noreply.github.com>
2019-01-04 19:53:42 -0500

    adding results structure, using same layout from goals and landing for consistency

--- tonic-fitness/src/components/Results.js
+++ tonic-fitness/src/components/Results.js
@@ -2,10 +2,27 @@ import React from 'react'
 
 const Results = (props) => {
     return (
-        <div>
+        <div id='results'>
            {props.weight}
            {props.goal}
            {props.by}
+
+            <section id="landing-container" className="flex-col">
+        <div className="display flex-row">
+          <article className="flex-col">
+              <i className="fas fa-weight"></i>
+            <p className="heading-text"> Calories today: </p>
+          </article>
+          <article className="flex-col">
+              <i className="fas fa-chart-line"></i>
+            <p className="heading-text"> Current Pace: </p>
+          </article>
+          <article className="flex-col">
+              <i className="fas fa-clock"></i>
+            <p className="heading-text"> By: </p>
+          </article>
+        </div>
+      </section>
         </div>
     )
 }
