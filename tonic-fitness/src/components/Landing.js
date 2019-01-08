52a9c26f2707720e69572194b2010b4fc651b739
ZumDeWald <zumdewald@gmail.com>
2019-01-07 22:00:38 -0600

    Touch-up wording on Landing

--- tonic-fitness/src/components/Landing.js
+++ tonic-fitness/src/components/Landing.js
@@ -5,22 +5,24 @@ const Landing = () => {
       <section id="landing-container" className="flex-col">
         <article className="landing-area">
           <div className="landing-box">
-            <h2 className="heading-text landing-header">Welcome to Tonic Fitness!</h2>
+            <h2 className="heading-text landing-box-words">Welcome to Tonic Fitness!</h2>
+            <p className="landing-box-words-d">We offer 3 easy to use features for creating goals, logging your caloric intake, and tracking you results.</p>
+            <p className="landing-box-words-d">You + these tools = Results!</p>
           </div>
           <div className="landing-box">
-            <h3 className="heading-text landing-header">3 easy steps to a better you!</h3>
+            <p className="heading-text landing-box-words">Sign up for a <em>free</em> account</p>
             <p className="landing-box-number"><strong>0</strong></p>
           </div>
           <div className="landing-box">
-            <p className="heading-text landing-header">Create weight based goals</p>
+            <p className="heading-text landing-box-words">Create weight based goals</p>
             <p className="landing-box-number"><strong>1</strong></p>
           </div>
           <div className="landing-box">
-            <p className="heading-text landing-header">Log calorie intake</p>
+            <p className="heading-text landing-box-words">Log calorie intake</p>
             <p className="landing-box-number"><strong>2</strong></p>
           </div>
           <div className="landing-box">
-            <p className="heading-text landing-header">Track your results</p>
+            <p className="heading-text landing-box-words">Track your results</p>
             <p className="landing-box-number"><strong>3</strong></p>
           </div>
         </article>
