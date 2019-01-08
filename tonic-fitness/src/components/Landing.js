2efc47401fd88d93e76fe0ba11205f61abb37d74
ZumDeWald <zumdewald@gmail.com>
2019-01-07 21:48:09 -0600

    Arrange tiles on Landing

--- tonic-fitness/src/components/Landing.js
+++ tonic-fitness/src/components/Landing.js
@@ -5,7 +5,20 @@ const Landing = () => {
       <section id="landing-container" className="flex-col">
         <article className="landing-area">
           <div className="landing-box">
-            <h2 className="heading-text landing-header">Welcome to Tonic Fitness!</h2>
+            <h3 className="heading-text landing-header">3 easy steps to a better you!</h3>
+            <p className="landing-box-number"><strong>0</strong></p>
+          </div>
+          <div className="landing-box">
+            <p className="heading-text landing-header">Create weight based goals</p>
+            <p className="landing-box-number"><strong>1</strong></p>
+          </div>
+          <div className="landing-box">
+            <p className="heading-text landing-header">Log calorie intake</p>
+            <p className="landing-box-number"><strong>2</strong></p>
+          </div>
+          <div className="landing-box">
+            <p className="heading-text landing-header">Track your results</p>
+            <p className="landing-box-number"><strong>3</strong></p>
           </div>
         </article>
       </section>
