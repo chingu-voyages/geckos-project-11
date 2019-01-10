d6c2e80e981c9ef6ca3a12d2d16fb4fc8601b845
ZumDeWald <zumdewald@gmail.com>
2019-01-09 21:28:46 -0600

    Make entire tile a Link in Dashboard

--- tonic-fitness/src/components/Dashboard.js
+++ tonic-fitness/src/components/Dashboard.js
@@ -2,33 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
-    return (
-      <section id="dashboard-container">
-        <article className="display flex-row">
-          <div className="landing-box flex-row">
-            <Link to="/goals">
-              <i className="fas fa-list-ol"></i>
-            </Link>
-            <p className="heading-text"> Create Goals
-            </p>
-          </div>
-          <div className="landing-box flex-row">
-            <Link to="/log">
-              <i className="fas fa-apple-alt"></i>
-            </Link>
-            <p className="heading-text"> Log Calories
-            </p>
-          </div>
-          <div className="landing-box flex-row">
-            <Link to="results">
-              <i className="fas fa-chart-bar"></i>
-            </Link>
-            <p className="heading-text"> See Results
-            </p>
-          </div>
-        </article>
-      </section>
-    )
-  }
+  return (
+    <section id="dashboard-container">
+      <article className="display flex-row">
+        <Link to="/goals" className="landing-box flex-row">
+          <i className="fas fa-list-ol"></i>
+          <p className="heading-text"> Create Goals
+          </p>
+        </Link>
+        <Link to="/log" className="landing-box flex-row">
+          <i className="fas fa-apple-alt"></i>
+          <p className="heading-text"> Log Calories
+          </p>
+        </Link>
+        <Link to="results" className="landing-box flex-row">
+          <i className="fas fa-chart-bar"></i>
+          <p className="heading-text"> See Results
+          </p>
+        </Link>
+      </article>
+    </section>
+  )
+}
 
 export default Dashboard;
