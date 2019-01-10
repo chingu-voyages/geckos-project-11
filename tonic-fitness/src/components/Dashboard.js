9a0b63f685d4494869a9cddc0c058bf1ed03467a
ZumDeWald <zumdewald@gmail.com>
2019-01-09 21:06:14 -0600

    Setup grid on Dashboard

--- tonic-fitness/src/components/Dashboard.js
+++ tonic-fitness/src/components/Dashboard.js
@@ -4,29 +4,29 @@ import { Link } from 'react-router-dom';
 const Dashboard = () => {
     return (
       <section id="dashboard-container" className="flex-col">
-        <div className="display flex-row">
-          <article className="flex-col">
+        <article className="display flex-row">
+          <div className="landing-box flex-col">
             <Link to="/goals">
               <i className="fas fa-list-ol"></i>
             </Link>
-            <p className="heading-text"> Set Goals
+            <p className="heading-text"> Goal
             </p>
-          </article>
-          <article className="flex-col">
+          </div>
+          <div className="landing-box flex-col">
             <Link to="/log">
               <i className="fas fa-apple-alt"></i>
             </Link>
-            <p className="heading-text"> Log Food
+            <p className="heading-text"> Log
             </p>
-          </article>
-          <article className="flex-col">
+          </div>
+          <div className="landing-box flex-col">
             <Link to="results">
               <i className="fas fa-chart-bar"></i>
             </Link>
-            <p className="heading-text"> See Results!
+            <p className="heading-text"> Results
             </p>
-          </article>
-        </div>
+          </div>
+        </article>
       </section>
     )
   }
