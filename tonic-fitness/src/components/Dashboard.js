c8152e537a826c416f978f52cc478aa781d01f0f
ZumDeWald <zumdewald@gmail.com>
2019-01-09 21:25:23 -0600

    Change Dashboard to row layout from column

--- tonic-fitness/src/components/Dashboard.js
+++ tonic-fitness/src/components/Dashboard.js
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-      <section id="dashboard-container" className="flex-col">
+      <section id="dashboard-container">
         <article className="display flex-row">
-          <div className="landing-box flex-col">
+          <div className="landing-box flex-row">
             <Link to="/goals">
               <i className="fas fa-list-ol"></i>
             </Link>
-            <p className="heading-text"> Goal
+            <p className="heading-text"> Create Goals
             </p>
           </div>
-          <div className="landing-box flex-col">
+          <div className="landing-box flex-row">
             <Link to="/log">
               <i className="fas fa-apple-alt"></i>
             </Link>
-            <p className="heading-text"> Log
+            <p className="heading-text"> Log Calories
             </p>
           </div>
-          <div className="landing-box flex-col">
+          <div className="landing-box flex-row">
             <Link to="results">
               <i className="fas fa-chart-bar"></i>
             </Link>
-            <p className="heading-text"> Results
+            <p className="heading-text"> See Results
             </p>
           </div>
         </article>
