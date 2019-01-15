595baedf60b1d4c3e2c22422ee69d36512e76358
ZumDeWald <zumdewald@gmail.com>
2019-01-15 06:20:31 -0600

    Layout and style footer

--- tonic-fitness/src/components/Footer.js
+++ tonic-fitness/src/components/Footer.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 const Footer = (props) => {
   return (
-    <footer id="footer" className="flex-row">
-      <p>Tonic-Fitness &copy; 2019</p>
-      <a href="https://github.com/chingu-voyages/geckos-project-11" target="_blank" rel="noopener noreferrer">
-      <p>App Source Code</p> </a>
+    <footer id="footer">
+      <section className="left">
+        <p className="footer-text"><strong>Built by:</strong></p>
+        <a href="https://github.com/kfarfan"
+           target="_blank" rel="noopener noreferrer">Kevin Farfan</a>
+        <a href="https://github.com/dionadk"
+           target="_blank" rel="noopener noreferrer">Diona Kurien</a>
+        <a href="https://github.com/ZumDeWald"
+           target="_blank" rel="noopener noreferrer">Eric Zumwalt</a>
+      </section>
+
+      <section className="right">
+        <p className="footer-text">&copy; Tonic-Fitness</p>
+        <a href="https://github.com/chingu-voyages/geckos-project-11" target="_blank" rel="noopener noreferrer">App Source Code</a>
+        <a href="https://chingu.io/" target="_blank" rel="noopener noreferrer">Chingu.io</a>
+        <a href="https://medium.com/chingu" target="_blank" rel="noopener noreferrer">Chingu on Medium</a>
+      </section>
+
     </footer>
   );
 }
