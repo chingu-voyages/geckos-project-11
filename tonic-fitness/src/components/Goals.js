51c23ae6b47a0d11b93e8e5790d4f2da1c2a4200
ZumDeWald <zumdewald@gmail.com>
2019-01-18 06:46:46 -0600

    Basic layout of goal entry for mobile

--- tonic-fitness/src/components/Goals.js
+++ tonic-fitness/src/components/Goals.js
@@ -33,36 +33,37 @@ class Goals extends Component  {
     const {UI} = this.state;
     return (
       <div id="goals">
-        <h1>My Goals</h1>
-        <input
-        className='textGoal' type='text' placeholder='Type your goal'
-        onKeyPress={this.updateGoals}>
-        </input>
-        <select className='selection' onClick={this.optionSelected}>
-          <option value='weight'>Current Weight</option>
-          <option value='goal' >Ideal Weight</option>
-          <option value='by'>By</option>
-          </select>
-
+        <h2 className="header">My Goals</h2>
+        <section className="enter-goal">
+          <input
+          className='text-goal' type='text' placeholder='Type your goal'
+          onKeyPress={this.updateGoals}>
+          </input>
+          <select className='selection' onClick={this.optionSelected}>
+            <option value='weight'>Current Weight</option>
+            <option value='goal' >Ideal Weight</option>
+            <option value='by'>By</option>
+            </select>
+        </section>
         <section id="landing-container" className="flex-col">
-        <div className="display flex-row">
-          <article className="flex-col">
-              <i className="fas fa-weight"></i>
-            <p className="heading-text"> Current Weight: <span className='weight'>{UI.weight}</span>
-            </p>
-          </article>
-          <article className="flex-col">
-              <i className="fas fa-running"></i>
-            <p className="heading-text"> Ideal Weight: <span className='goal'>{UI.goal}</span>
-            </p>
-          </article>
-          <article className="flex-col">
-              <i className="fas fa-clock"></i>
-            <p className="heading-text"> By: <span className='by'>{UI.by}</span>
-            </p>
-          </article>
-        </div>
-      </section>
+          <div className="flex-row">
+            <article className="flex-col">
+                <i className="fas fa-weight"></i>
+              <p className="heading-text"> Current Weight: <span className='weight'>{UI.weight}</span>
+              </p>
+            </article>
+            <article className="flex-col">
+                <i className="fas fa-running"></i>
+              <p className="heading-text"> Ideal Weight: <span className='goal'>{UI.goal}</span>
+              </p>
+            </article>
+            <article className="flex-col">
+                <i className="fas fa-clock"></i>
+              <p className="heading-text"> By: <span className='by'>{UI.by}</span>
+              </p>
+            </article>
+          </div>
+        </section>
       </div>
     );
   }
