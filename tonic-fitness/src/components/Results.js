6f96e006521e97e99f1bf2f46e079ba8040fd9f6
ZumDeWald <zumdewald@gmail.com>
2019-01-19 11:49:48 -0600

    BG and general layout in Results

--- tonic-fitness/src/components/Results.js
+++ tonic-fitness/src/components/Results.js
@@ -11,21 +11,19 @@ const Results = (props) => {
     <div id="results">
       <h2 className="header">Maintenance 2000 calories</h2>
 
-      <section id="landing-container" className="flex-col">
-        <div className="display flex-row">
-          <article className="flex-col">
-              <i className="fas fa-weight"></i>
-            <p className="heading-text"> Calories today: {totalCalories} </p>
-          </article>
-          <article className="flex-col">
-              <i className="fas fa-chart-line"></i>
-            <p className="heading-text"> Current Pace: {pace} days</p>
-          </article>
-          <article className="flex-col">
-              <i className="fas fa-clock"></i>
-            <p className="heading-text"> By: </p>
-          </article>
-        </div>
+      <section className="result-display">
+        <article className="flex-col">
+            <i className="fas fa-weight"></i>
+          <p className="heading-text"> Calories today: {totalCalories} </p>
+        </article>
+        <article className="flex-col">
+            <i className="fas fa-chart-line"></i>
+          <p className="heading-text"> Current Pace: {pace} days</p>
+        </article>
+        <article className="flex-col">
+            <i className="fas fa-clock"></i>
+          <p className="heading-text"> By: </p>
+        </article>
       </section>
     </div>
   )
