3a72a5d0426bbe05d5a352e720536b6bc4ca414c
ZumDeWald <zumdewald@gmail.com>
2019-01-27 22:15:33 -0600

    Trying to build Chart.js

--- tonic-fitness/src/components/Results.js
+++ tonic-fitness/src/components/Results.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Chart from 'chart.js';
 
 const Results = (props) => {
   let maintenance = 2000;
@@ -7,11 +8,56 @@ const Results = (props) => {
   let target = (props.weight - props.goal) * 3500;
   let pace = Math.round(target/defecit);
 
+  //Define ctx for Chart
+  let ctx = document.getElementById("results-chart");
+
+  //Setup Chart
+  let resultChart = new Chart (ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      datasets: [{
+        label: "Daily Goal",
+        data: ["2000", "2000", "2000", "2000", "2000", "2000", "2000",],
+        backgroundColor: [
+          "green",
+          "green",
+          "green",
+          "green",
+          "green",
+          "green",
+          "green"
+        ],
+        borderColor: [
+          "red",
+          "red",
+          "red",
+          "red",
+          "red",
+          "red",
+          "red"
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+
   return (
     <div id="results">
       <h2 className="header">Maintenance 2000 calories</h2>
 
       <section className="result-display">
+        <canvas id="results-chart" width="600" height="600">
+        </canvas>
         <article className="results-box flex-col">
             <i className="fas fa-weight"></i>
           <p className="heading-text dark-text"> Calories today: {totalCalories} </p>
