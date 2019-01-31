b379047f2b945430b126acc0c4051b416923310b
ZumDeWald <zumdewald@gmail.com>
2019-01-30 21:26:17 -0600

    Add ternary to show/hide icon links in Nav

--- tonic-fitness/src/components/Nav.js
+++ tonic-fitness/src/components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = (props) => {
+
   return (
     <nav id="nav-container">
       <header id="header">
@@ -9,18 +10,23 @@ const Nav = (props) => {
               className="top-left-title">
           <h1 className="title">Tonic Fitness</h1>
         </Link>
-        <section className="icons">
-          <Link to="/goals">
-            <i className="fas fa-list-ol link-icons"></i>
-          </Link>
-          <Link to="/log">
-            <i className="fas fa-apple-alt link-icons"></i>
-          </Link>
-          <Link to="/results">
-            <i className="fas fa-chart-bar link-icons"></i>
-          </Link>
-        </section>
-        {/* Ternary to display Login options if no user logged in or Welcome {name} if user is signed in */
+        {/* First Ternary to display Login options if no user logged in or Welcome {name} if user is signed in */
+          (!props.currentUser) ?
+          (<section className="icons hide"></section>) :
+          (<section className="icons">
+            <Link to="/goals">
+              <i className="fas fa-list-ol link-icons"></i>
+            </Link>
+            <Link to="/log">
+              <i className="fas fa-apple-alt link-icons"></i>
+            </Link>
+            <Link to="/results">
+              <i className="fas fa-chart-bar link-icons"></i>
+            </Link>
+          </section>)
+        /* End of First Ternary */}
+
+        {/* Second Ternary to display Login options if no user logged in or Welcome {name} if user is signed in */
           (!props.currentUser) ?
           (<div id="top-right-of-nav-login" className="login">
             <Link to="/login/signup"
@@ -34,7 +40,7 @@ const Nav = (props) => {
             <button className="logout"
                   onClick={props.handleLogoutUser}>Logout</button>
           </div>)
-            }
+            /* End of Second Ternary */}
       </header>
     </nav>
   );
